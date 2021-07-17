3d767e82753a2f1cf8014a303da8232f52fe9ccc
Eric Blau <blau@mcs.anl.gov>
2021-07-17 14:14:29 -0500

    fix typo

--- bin/route_globus_v3.py
+++ bin/route_globus_v3.py
@@ -521,7 +521,7 @@ class Router():
                     Description.blank_line()
                 Description.append('- Usage documentation: https://www.globus.org/data-transfer')
                 Description.blank_line()
-                Description.append('- Globus UUID: {}'.format(item.get('id'))
+                Description.append('- Globus UUID: {}'.format(item.get('id')))
                 globuskeywords = item.get('keywords')
                 if globuskeywords:
                     keywords = globuskeywords+",Globus,File Transfer"
