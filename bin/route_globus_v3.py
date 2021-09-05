946fafe560df5d1e46c85ed8ff39da04ada534e9
John-Paul Navarro <navarro@anl.gov>
2021-09-05 14:27:05 -0500

    See CHANGELOG tag-0.18-20210905b

--- bin/route_globus_v3.py
+++ bin/route_globus_v3.py
@@ -345,7 +345,7 @@ class Router():
             };
         SCOPESTRING = 'urn:globus:auth:scope:transfer.api.globus.org:all'
 
-        nac = globus_sdk.NativeAppAuthClient(CLIENT_ID)
+        nac = globus_sdk.NativeAppAuthClient(self.config['GLOBUS_CLIENT_ID'])
         authorizer = globus_sdk.RefreshTokenAuthorizer(
                                          REFRESH_TOKEN, nac)
 
